293b706e5d4cb6701a8dc29f01296c6aecd83a2c
Jan Larres <jan@majutsushi.net>
2025-01-02 20:52:19 +1300

    Add static analysis config

---
+++ __builtins__.pyi
@@ -0,0 +1,13 @@
+from typing import overload
+
+from calibre.utils.resources import get_image_path, get_path
+
+def load_translations() -> None: ...
+def _(message: str) -> str: ...
+@overload
+def get_resources(name: str) -> bytes: ...
+@overload
+def get_resources(list_of_names: list[str]) -> dict[str, bytes]: ...
+
+I = get_image_path  # noqa: E741
+P = get_path
